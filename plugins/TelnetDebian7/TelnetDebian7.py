1eafc84c31128561a7a49fefaae8cbe5ee895afa
foospidy <foospidy@users.noreply.github.com>
2015-10-13 06:49:58 -0400

    added interactive shell upon successful login, limited commands

--- plugins/TelnetDebian7/TelnetDebian7.py
+++ plugins/TelnetDebian7/TelnetDebian7.py
@@ -16,7 +16,7 @@ class TelnetDebian7(protocol.Protocol): ### Set custom protocol class name
 	session     = None
 	
 	### START CUSTOM VARIABLES ###############################################################
-	state = 'NO_UNAME'
+	state = 'INIT'
 	uname = None
 	pword = None
 	
@@ -28,7 +28,6 @@ class TelnetDebian7(protocol.Protocol): ### Set custom protocol class name
 
 		### START CUSTOM CODE ####################################################################
 		self.tx('Debian GNU/Linux 7\r\n')
-		self.tx('Login: ')
 		
 		##########################################################################################
 
@@ -36,25 +35,77 @@ class TelnetDebian7(protocol.Protocol): ### Set custom protocol class name
 		self.rx(data)
 
 		### START CUSTOM CODE ####################################################################
-		if 'NO_UNAME' == self.state:
-			self.uname = data.rstrip()
-			self.state = 'NO_PWORD'
-			self.tx('Password: ')
+		if 'INIT' == self.state:
+			self.state = 'NO_UNAME'
+			self.tx('Login: ')
+		elif 'NO_UNAME' == self.state:
+			self.uname = data.strip()
+			if len(self.uname) > 0:
+				self.state = 'NO_PWORD'
+				self.tx('Password: ')
+			else:
+				self.tx('Login: ')
 		elif 'NO_PWORD' == self.state:
 			self.pword = data.rstrip()
-			if 'Pa$$word' == self.pword:
-				self.tx('WARNING: System error, closing connection.\n')
-				self.transport.loseConnection()
+			if self.authn(self.pword):
+				self.state = 'COMMAND'
+				self.tx(self.uname + '$ ')
 			else:
 				self.state = 'NO_UNAME'
 				self.tx('Login incorrect\n')
 				self.tx('Login: ')
+		elif 'COMMAND' == self.state:
+			# parse command
+			commandParts = str(data).split()
+
+			if 0 == len(commandParts):
+				self.tx(self.uname + '$ ')
+			else:
+				command      = commandParts[0].lower()
+				args         = commandParts[1:]
+
+				try:
+					method = getattr(self, 'do_' + command)
+				except AttributeError, e:
+					self.tx('- bash: ' + command + ': command not found\r\n')
+					self.tx(self.uname + '$ ')
+				else:
+					try:
+						method(*args)
+						self.tx(self.uname + '$ ')
+					except Exception, e:
+						self.tx('WARNING: System error, closing connection.\n')
+						self.transport.loseConnection()
+		else:
+			# should never reach here
+			self.tx('WARNING: System error, closing connection.\n')
+			self.transport.loseConnection()
 
 		##########################################################################################
 
 	### START CUSTOM FUNCTIONS ###################################################################
-
-			
+	def authn(self, password=None):
+		authenticated = False
+		
+		if 'admin' == password or 'password' == password or 'root' == password or 'password' == password:
+			authenticated = True
+		
+		return authenticated
+	
+	def do_exit(self, args=None):
+		self.transport.loseConnection()
+	
+	def do_whoami(self, args=None):
+		self.tx(self.uname)
+	
+	def do_uname(self, args=None):
+		self.tx('Linux bitminer 3.16.0-4-amd64 #1 SMP Debian 3.16.7-ckt9-3~deb8u1 (2015-04-24) x86_64 GNU/Linux\r\n')
+	
+	def do_which(self, args=None):
+		if None != args:
+			command = args.split()
+			self.tx('/bin/' + command[0] + '\r\n')
+	
 	##############################################################################################
 
 	def connect(self):
