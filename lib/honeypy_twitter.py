2cdfda4b3893299d1c4f2bf2962cc518fd2dfc4f
Px Mx <foospidy@users.noreply.github.com>
2016-03-06 11:19:39 -0500

    animus threat bot is dead

--- lib/honeypy_twitter.py
+++ lib/honeypy_twitter.py
@@ -13,16 +13,11 @@ def post_tweet(honeypycfg, service, clientip):
 	cs = honeypycfg.get('twitter', 'consumersecret')
 	ot = honeypycfg.get('twitter', 'oauthtoken')
 	os = honeypycfg.get('twitter', 'oauthsecret')
-	tb = honeypycfg.get('twitter', 'ask_animus') # threat bot
 
 	t        = Twitter(auth=OAuth(ot, os, ck, cs))
 	nodename = honeypycfg.get('twitter', 'nodename')
-	animus   = '';
-	
-	if('Yes' == tb):
-		animus = ' @threatbot'
 	
 	try:
-		t.statuses.update(status=nodename + ': #' + service + ' Possible '  + service + ' attack from ' + clientip + ' https://riskdiscovery.com/honeydb/#host/' + clientip + animus)
+		t.statuses.update(status=nodename + ': #' + service + ' Possible '  + service + ' attack from ' + clientip + ' https://riskdiscovery.com/honeydb/#host/' + clientip)
 	except Exception, err:
 		log.msg('Error posting to Twitter: %s' % err)
