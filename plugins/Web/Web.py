56fb3d955297d92ecafa22d66db0a3ebdf8693b2
foospidy <phillip@foospidy.com>
2016-05-15 09:55:06 -0400

    add simple web plugin

---
+++ plugins/Web/Web.py
@@ -0,0 +1,60 @@
+# HoneyPy Copyright (C) 2013-2016 foospidy
+# https://github.com/foospidy/HoneyPy
+# See LICENSE for details
+
+from twisted.internet import protocol, reactor, endpoints
+from twisted.python import log
+import uuid
+
+### START CUSTOM IMPORTS ###
+
+############################
+
+class Web(protocol.Protocol): ### Set custom protocol class name
+	localhost   = None
+	remote_host = None
+	session     = None
+
+	### START CUSTOM VARIABLES ###############################################################
+	
+	##########################################################################################
+	
+	# handle events
+	def connectionMade(self):
+		self.connect()
+
+		### START CUSTOM CODE ####################################################################
+		
+		##########################################################################################
+
+	def dataReceived(self, data):
+		self.rx(data)
+
+		### START CUSTOM CODE ####################################################################
+		response = 'HTTP/1.1 200 OK\nServer: Apache/2.4.10 (Debian)\nConnection: close\nContent-Type: text/html\n\nOK!'
+		self.tx(response)
+
+		##########################################################################################
+
+	### START CUSTOM FUNCTIONS ###################################################################
+
+	##############################################################################################
+
+	def connect(self):
+		self.local_host  = self.transport.getHost()
+		self.remote_host = self.transport.getPeer()
+		self.session     = uuid.uuid1()
+		log.msg('%s %s CONNECT %s %s %s %s %s' % (self.session, self.remote_host.type, self.local_host.host, self.local_host.port, self.factory.name, self.remote_host.host, self.remote_host.port))
+
+	def tx(self, data):
+		log.msg('%s %s TX %s %s %s %s %s %s' % (self.session, self.remote_host.type, self.local_host.host, self.local_host.port, self.factory.name, self.remote_host.host, self.remote_host.port, data.encode("hex")))
+		self.transport.write(data)
+
+	def rx(self, data):
+		log.msg('%s %s RX %s %s %s %s %s %s' % (self.session, self.remote_host.type, self.local_host.host, self.local_host.port, self.factory.name, self.remote_host.host, self.remote_host.port, data.encode("hex")))
+
+class pluginFactory(protocol.Factory):
+	protocol = Web ### Set protocol to custom protocol class name
+	
+	def __init__(self, name=None):
+		self.name = name or 'HoneyPy'
