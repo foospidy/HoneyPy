cbcdfff4aa35c65f95a2e6527a0a2641cb30973a
Px Mx <foospidy@users.noreply.github.com>
2016-05-16 15:13:03 -0400

    Create honeypy_slack.py

---
+++ lib/honeypy_slack.py
@@ -0,0 +1,21 @@
+# HoneyPy Copyright (C) 2013-2016 foospidy
+# https://github.com/foospidy/HoneyPy
+# See LICENSE for details
+# HoneyPy Slack Integration
+
+import sys
+import requests
+from twisted.python import log
+
+# prevent creation of compiled bytecode files
+sys.dont_write_bytecode = True
+
+def post_slack(honeypycfg, service, clientip):
+    headers = { 'Content-type': 'application/json', 'User-Agent': honeypycfg.get('honeypy', 'useragent') }
+    data    = '{"text": "' + honeypycfg.get('honeypy', 'nodename') + ': Possible ' + service + ' attack from ' + clientip + ' <https://riskdiscovery.com/honeydb/#host/' + clientip + '>"}'
+
+    url     = honeypycfg.get('slack', 'webhook_url')
+    r       = requests.post(url, headers=headers, data=data)
+
+    if r.status_code != requests.codes.ok:
+        log.msg('Error posting to Slack: %s' % str(r.status_code))
