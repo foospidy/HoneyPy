2bb7f7e2ef716ed1a7a42cc62cfa1d53dd08f7f1
Matt Joyce <matt.joyce@gmail.com>
2018-03-09 15:55:48 +1100

    Add custom headers for Sumologic logger Custom Source Name	X-Sumo-Name	Desired source name Custom Source Host	X-Sumo-Host	Desired host name Custom Source Category	X-Sumo-Category	Desired source category

--- loggers/sumologic/honeypy_sumologic.py
+++ loggers/sumologic/honeypy_sumologic.py
@@ -12,13 +12,23 @@ from twisted.python import log
 # prevent creation of compiled bytecode files
 sys.dont_write_bytecode = True
 
-def post_sumologic(useragent, url, date, time, date_time, millisecond, session, protocol, event, local_host, local_port, service, remote_host, remote_port, data):
+def post_sumologic(useragent, custom_source_host, custom_source_name, custom_source_category, url, date, time, date_time, millisecond, session, protocol, event, local_host, local_port, service, remote_host, remote_port, data):
 	h = hashlib.md5()
 	h.update(data)
 
 	date_time = datetime.strptime(date_time,"%Y-%m-%d %H:%M:%S").isoformat()
 
 	headers = { 'User-Agent': useragent, "Content-Type": "application/json" }
+
+	if custom_source_host :
+		headers.update({'X-Sumo-Host' : custom_source_host})
+
+        if custom_source_name :
+                headers.update({'X-Sumo-Name' : custom_source_name})
+
+        if custom_source_category :
+                headers.update({'X-Sumo-Category' : custom_source_category})
+
 	# applying [:-3] to time to truncate millisecond
 	data    = {
 		'date': date,
@@ -39,7 +49,7 @@ def post_sumologic(useragent, url, date, time, date_time, millisecond, session, 
 	}
 
 	try:
-		r       = requests.post(url, headers=headers, json=data, verify=False, timeout=3)
+		r       = requests.post(url, headers=headers, json=data, verify=True, timeout=3)
 		page    = r.text
 
 		log.msg('Post event to sumologic, response: %s' % (str(page).strip()))
