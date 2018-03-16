69ac97250d8ed000d0dd2518f69cfc964cbfd2eb
Matt Joyce <matt.joyce@gmail.com>
2018-03-17 09:55:01 +1100

    Pylint fixes

--- loggers/logstash/honeypy_logstash.py
+++ loggers/logstash/honeypy_logstash.py
@@ -45,53 +45,49 @@ def process(config, section, parts, time_parts, useragent):
         if len(parts) == 11:
             parts.append('')  # no data for CONNECT events
 
-        post(useragent, config.get(section, 'host'), config.get(section, 'port'), parts[0], time_parts[0], parts[0] + ' ' + time_parts[0], time_parts[1], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8], parts[9], parts[10], parts[11])
+        post(config.get(section, 'host'), config.get(section, 'port'), parts[0], time_parts[0], parts[0] + ' ' + time_parts[0], time_parts[1], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8], parts[9], parts[10], parts[11])
     else:
         # UDP splits differently (see comment section above)
         if len(parts) == 12:
             parts.append('')  # no data sent
 
-        post(useragent, config.get(section, 'host'), config.get(section, 'port'), parts[0], time_parts[0], parts[0] + ' ' + time_parts[0], time_parts[1], parts[4], parts[5], parts[6], parts[7], parts[8], parts[9], parts[10], parts[11], parts[12])
+        post(config.get(section, 'host'), config.get(section, 'port'), parts[0], time_parts[0], parts[0] + ' ' + time_parts[0], time_parts[1], parts[4], parts[5], parts[6], parts[7], parts[8], parts[9], parts[10], parts[11], parts[12])
 
 
-def post(useragent, host, port, date, time, date_time, millisecond, session, protocol, event, local_host, local_port, service, remote_host, remote_port, data):
-	# post events to honeydb logger
-	h = hashlib.md5()
-	h.update(data)
+def post(host, port, date, time, date_time, millisecond, session, protocol, event, local_host, local_port, service, remote_host, remote_port, data):
+    # post events to honeydb logger
+    h = hashlib.md5()
+    h.update(data)
 
-	headers = { 'User-Agent': useragent }
-	# applying [:-3] to time to truncate millisecond
-	data = {
-		'date': date,
-		'time': time,
-		'date_time': date_time,
-		'millisecond': str(millisecond)[:-3],
-		'session': session,
-		'protocol': protocol,
-		'event': event,
-		'local_host': local_host,
-		'local_port': local_port,
-		'service': service,
-		'remote_host': remote_host,
-		'remote_port': remote_port,
-		'data': data,
-		'bytes': str(len(data)),
-		'data_hash': h.hexdigest()
-	}
+    # applying [:-3] to time to truncate millisecond
+    data = {
+        'date': date,
+        'time': time,
+        'date_time': date_time,
+        'millisecond': str(millisecond)[:-3],
+        'session': session,
+        'protocol': protocol,
+        'event': event,
+        'local_host': local_host,
+        'local_port': local_port,
+        'service': service,
+        'remote_host': remote_host,
+        'remote_port': remote_port,
+        'data': data,
+        'bytes': str(len(data)),
+        'data_hash': h.hexdigest()
+    }
 
-	try:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, int(port)))
+        bytes_sent = sock.send(json.dumps(data))
+        sock.close()
 
-		sock.connect((host, int(port)))
-		
-		bytes_sent = sock.send(json.dumps(data))
+        log.msg('Post event to logstash! (%s bytes)' % str(bytes_sent))
 
-		sock.close()
+    except socket.error, msg:
+        log.msg('[ERROR] post_logstash, socket.error: %s' % msg[1])
 
-		log.msg('Post event to logstash! (%s bytes)' % str(bytes_sent))
-
-	except socket.error, msg:
-			log.msg('[ERROR] post_logstash, socket.error: %s' % msg[1])
-
-	except Exception as e:
-		log.msg('Error posting to logstash: %s' % (str(e.message).strip()))
+    except Exception as e:
+        log.msg('Error posting to logstash: %s' % (str(e.message).strip()))
